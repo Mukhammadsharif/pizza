4b4474c6a4647f9a6555b428908f1dd865263fe7
Mukhammadsharif <80579706+Mukhammadsharif@users.noreply.github.com>
2023-11-27 14:03:39 +0500

    first commit

---
+++ source/details/Icons.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Svg, {
+  Circle,
+  ClipPath,
+  Defs,
+  G,
+  LinearGradient,
+  Mask,
+  Path,
+  Pattern,
+  Rect,
+  Stop,
+  Use,
+  Image,
+  Ellipse,
+} from 'react-native-svg';
